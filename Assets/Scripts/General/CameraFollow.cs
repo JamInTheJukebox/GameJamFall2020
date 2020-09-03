934d088f7813cf51d7459567a6ba0e86556ca779
JamInTheJukebox <44655457+JamInTheJukebox@users.noreply.github.com>
2020-09-03 17:36:16 -0500

    Added Basic movement
    - Movement: Linear and force based
    - Camera: Always Target and Allow Threshold

---
+++ Assets/Scripts/General/CameraFollow.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    //private Rigidbody2D rb_Target;
+    [SerializeField] Camera Follower;
+
+    private Vector3 velocity = Vector3.zero;
+    public float TimeToReach = 0.3f;
+    private float Z_Value;                      // Keep Z-Value constant!
+
+    /*
+     * The threshold mode only
+     */
+    private Vector3 EndPos;
+    public Vector2 FollowOffset;
+    private Vector2 threshold;
+
+    [SerializeField] bool DrawBorder = true;
+
+    [SerializeField] E_mode CurrentMode;
+    public enum E_mode
+    {
+        AlwaysTarget = 0,
+        AllowThreshold = 1
+    };
+
+    private void Awake()
+    {
+        if(Follower == null)
+           Follower = Camera.main;
+        if (CurrentMode == E_mode.AllowThreshold)
+        {
+            /*
+            if (Target.GetComponent<Rigidbody2D>() == null)
+            {
+                rb_Target = Target.gameObject.AddComponent<Rigidbody2D>();
+                Debug.LogWarning("CameraFollower.cs: There is not a rigidbody attached to the Target.");
+            }
+            rb_Target = Target.GetComponent<Rigidbody2D>();
+            */
+        }
+
+        Z_Value = transform.position.z;
+
+    }
+
+    private void Start()
+    {
+        threshold = RetrieveThreshold();
+    }
+    private void Update()
+    {
+        if(CurrentMode == E_mode.AllowThreshold)
+            RetreiveTargetPos();
+
+
+    }
+
+    private void RetreiveTargetPos()
+    {
+        Vector2 TargetPos = Target.position;
+        Vector3 FollowerPos = transform.position;
+        float X_DeltaPos = Vector2.Distance(Vector2.right * FollowerPos.x, Vector2.right * TargetPos.x);
+        float Y_DeltaPos = Vector2.Distance(Vector2.up * FollowerPos.y, Vector2.up * TargetPos.y);
+        if (Mathf.Abs(X_DeltaPos) >= threshold.x)
+        {
+            FollowerPos.x = TargetPos.x;
+        }
+        if (Mathf.Abs(Y_DeltaPos) >= threshold.y)
+        {
+            FollowerPos.y = TargetPos.y;
+        }
+
+        EndPos = FollowerPos;
+    }
+
+    private Vector3 RetrieveThreshold()
+    {
+        Rect Aspect = Follower.pixelRect;
+        Vector2 t = new Vector2(Follower.orthographicSize * Aspect.width / Aspect.height, Follower.orthographicSize);
+        t.x -= FollowOffset.x;
+        t.y -= FollowOffset.y;
+        return t;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(CurrentMode == E_mode.AllowThreshold && DrawBorder)
+        {
+            Gizmos.color = Color.blue;
+            Vector2 border = RetrieveThreshold();
+            Gizmos.DrawWireCube(transform.position, new Vector3(2 * border.x, 2 * border.y, 1));
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (Target == null) { return; }
+        Vector3 Start_vec = new Vector3(transform.position.x, transform.position.y, Z_Value);
+        Vector3 End_vec = Vector3.zero;
+        if (CurrentMode == E_mode.AlwaysTarget)
+            End_vec = new Vector3(Target.position.x, Target.position.y, Z_Value);
+        else if (CurrentMode == E_mode.AllowThreshold)
+            End_vec = new Vector3(EndPos.x,EndPos.y,Z_Value);
+        SmoothDamp(Start_vec, End_vec);
+    }
+    private void SmoothDamp(Vector3 Start, Vector3 End)
+    {
+        transform.position = Vector3.SmoothDamp(Start, End, ref velocity, TimeToReach);
+    }
+}
---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float LinearDrag = 0.6f;
+
+    [Header("Walking/Running")]
+    public float WalkAcceleration = 5f;
+    public float MaxWalkSpeed = 4f;
+
+    public float RunAcceleration = 10f;
+    public float MaxRunSpeed = 10;
+
+    private float Dir = 0;
+    public E_MovementType Movement_Type = E_MovementType.Linear;
+    private bool FacingRight = true;
+
+    public enum E_MovementType
+    {
+        Linear = 0,
+        Force = 1,
+    }
+
+    [Header("Jumping")]
+    [SerializeField] LayerMask GroundLayer = 1 << 8;
+    [SerializeField] bool onGround = false;
+    public float GroundLength = 0.6f;
+    [SerializeField] Vector3 RayCastOffset = Vector3.zero;
+    public float JumpImpulse = 15f;
+    [SerializeField] float gravity = 1;
+    [SerializeField] float FallMultiplier = 4;
+    [SerializeField] float JumpDelay = 0.25f;
+    [SerializeField] float MaxFallSpeed = 10f;
+    private float JumpTimer = 0;
+
+    [Header("DebugTools")]
+    [SerializeField] bool DrawGroundCheck = false;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Dir = Input.GetAxis("Horizontal");
+        onGround = Physics2D.Raycast(transform.position + RayCastOffset, Vector2.down, GroundLength, GroundLayer)
+            || Physics2D.Raycast(transform.position - RayCastOffset, Vector2.down, GroundLength, GroundLayer);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpTimer = Time.time + JumpDelay;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // You can easily detach
+        HorizontalMovement();
+        if (JumpTimer > Time.time && onGround)
+        {
+            Jump();
+        }
+        ModifyPhysics();
+        
+        if(rb.velocity.y <= -MaxFallSpeed)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -MaxFallSpeed);
+        }
+    }
+    private void HorizontalMovement()
+    {
+        float MaxSpeed = Input.GetKey(KeyCode.LeftShift) ? MaxRunSpeed : MaxWalkSpeed; 
+        float MoveSpeed = Input.GetKey(KeyCode.LeftShift) ? RunAcceleration : WalkAcceleration;
+        if (Movement_Type == E_MovementType.Linear)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift)) { MoveSpeed = MaxSpeed; }
+            Vector2 newMove = new Vector2(Dir * MoveSpeed, rb.velocity.y);
+            rb.velocity = newMove;
+        }
+        else if (Movement_Type == E_MovementType.Force)                                 // Force-Based movement will be necessary for adding hazards that slow or speed you up.
+        {
+            Vector2 AccumulativeForce = Vector2.right * MoveSpeed * Dir;
+            if (Mathf.Abs(rb.velocity.x) >= MaxSpeed)
+            {
+                rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * MaxSpeed, rb.velocity.y);
+                AccumulativeForce = Vector2.zero;
+            }
+            rb.AddForce(AccumulativeForce);
+
+
+            if (Dir > 0 && !FacingRight || Dir < 0 && FacingRight)
+            {
+                FlipDirection();
+            }
+        }
+    }
+
+    private void FlipDirection()
+    {
+        FacingRight = !FacingRight;
+        transform.rotation = Quaternion.Euler(0, FacingRight ? 0 : 180, 0);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(Vector2.up * JumpImpulse, ForceMode2D.Impulse);
+        JumpTimer = 0;
+    }
+
+    private void ModifyPhysics()
+    {
+        bool ChangingDirections = (Dir > 0 && rb.velocity.x < 0) || (Dir < 0 && rb.velocity.x > 0);
+        if (onGround)
+        {
+            if (Mathf.Abs(Dir) < 0.4f || ChangingDirections)
+            {
+                rb.drag = LinearDrag;
+            }
+            else { rb.drag = 0; }
+
+            rb.gravityScale = 0;
+
+        }
+        else
+        {
+            rb.gravityScale = gravity;
+            rb.drag = LinearDrag * 0.15f;
+            if (rb.velocity.y < 0)
+            {
+                rb.gravityScale = gravity * FallMultiplier;
+            }
+            else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+            {
+                rb.gravityScale = gravity * (FallMultiplier / 2);
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        if(DrawGroundCheck)
+        {
+            Gizmos.color = (onGround) ? Color.green : Color.red;
+            Vector3 CurrentPos = transform.position;
+            Gizmos.DrawLine(CurrentPos + RayCastOffset, CurrentPos + Vector3.down * GroundLength + RayCastOffset);
+            Gizmos.DrawLine(CurrentPos - RayCastOffset, CurrentPos + Vector3.down * GroundLength - RayCastOffset);
+
+        }
+    }
+}
