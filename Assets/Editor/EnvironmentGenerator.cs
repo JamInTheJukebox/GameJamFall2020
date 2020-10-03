ff7b53810dc3c37b5fbdbd4955d8594eabae8526
JamInTheJukebox <44655457+JamInTheJukebox@users.noreply.github.com>
2020-10-02 20:37:28 -0500

    Revert "Revert "Hierarchy organization and Environment Generator Tool""
    This reverts commit 6f4313713cdff0633eba6fa9fd2071258537a79a.

---
+++ Assets/Editor/EnvironmentGenerator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+[CustomEditor(typeof(Environment))]
+public class EnvironmentGenerator : Editor
+{
+    public override void OnInspectorGUI()       // overrides default inspector view
+    {
+        DrawDefaultInspector();
+
+        Environment Plat = (Environment)target;
+
+        if(GUILayout.Button("Create Platforms"))
+        {
+            Plat.SpawnPlatforms();
+        }
+    }
+}
