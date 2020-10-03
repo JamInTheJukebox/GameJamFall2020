198ddb0697b3ecce9a6caeec485a4be5da94d832
JamInTheJukebox <44655457+JamInTheJukebox@users.noreply.github.com>
2020-10-02 23:49:57 -0500

    Revert "Resolved?"
    This reverts commit 575b3d9587393b8b89ca597b932f13a4235e25b5.

---
+++ Assets/Editor/HierarchyLabel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+
+namespace Project2020
+{
+    using UnityEngine;
+    [InitializeOnLoad]
+    public class HierarchyLabel : MonoBehaviour
+    {
+        static HierarchyLabel()
+        {
+            EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
+        }
+
+        static void HierarchyWindowItemOnGUI(int instanceID, Rect SelectionRect)
+        {
+            GameObject obj = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+            if(obj != null && obj.name.StartsWith("---", System.StringComparison.Ordinal))
+            {
+                EditorGUI.DrawRect(SelectionRect, Color.grey);
+                EditorGUI.DropShadowLabel(SelectionRect, obj.name.Replace("-", "").ToString());
+            }
+        }
+    }
+}
+
