d007f0ce9128c2cf00e0b1a241205c59f94a1016
JamInTheJukebox <44655457+JamInTheJukebox@users.noreply.github.com>
2020-11-15 02:16:35 -0600

    More organization and other map tweaks

---
+++ Assets/Scripts/UI/Slot_Machine/Object_Toggler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Toggler : MonoBehaviour
+{
+
+    public enum E_CurrentItem
+    {
+        
+        NoItem = 0,                                        // walking, running, jumping    
+        Blue = 1,
+        Green = 2,
+        Yellow = 3,
+        Red = 4                            // Might implement it, maybe not. IDK
+    }
+}
