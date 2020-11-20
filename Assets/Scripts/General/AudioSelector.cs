13c6f4b9e34eb6f39255c3e9bc2cb7111f1844d5
JamInTheJukebox <44655457+JamInTheJukebox@users.noreply.github.com>
2020-11-19 19:20:24 -0600

    Merge!

--- Assets/Scripts/General/AudioSelector.cs
+++ Assets/Scripts/General/AudioSelector.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class AudioSelector : MonoBehaviour
 {
     public AudioClip AwakeClip;
+    public AudioClip Clip2;
+
+    public float loopTime = 2f;
+    int iterator = 0;
 
     private void Start()
     {
         AudioManager.Instance.PlayMusicwithFade(AwakeClip);
+        StartCoroutine(LoopMusic());
+    }
+
+    private IEnumerator LoopMusic()
+    {
+        while (true)
+        {
+            AudioClip nextClip = (iterator == 0) ? (Clip2) : AwakeClip;
+            iterator = (iterator == 0) ? 1 : 0;
+            yield return new WaitForSeconds(loopTime);
+            AudioManager.Instance.PlayMusicwithCrossFade(nextClip,1);
+        }
     }
 }
